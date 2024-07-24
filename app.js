c2634545e14ea8611306a51c032e18f731cf1d92
pypymi <dr.mizoguchi@gmail.com>
2024-07-24 10:53:14 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let text = document.querySelector(".text");
+
+let shadow = "";
+
+for (let i = 0; i < 30; i++) {
+  shadow += (shadow ? "," : "") + -i * 1 + "px " + i * 1 + "px 0 #d9d9d9";
+}
+
+text.style.textShadow = shadow;
